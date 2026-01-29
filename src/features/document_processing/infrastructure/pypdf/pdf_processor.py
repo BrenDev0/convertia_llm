07e64ee0042063efa4fbb2960cc39d5ba27d8bd2
Xplorers-backend <brendan.soullens@gmail.com>
2026-01-29 16:22:22 -0600

    WIP: chunker

--- src/features/document_processing/infrastructure/pypdf/pdf_processor.py
+++ src/features/document_processing/infrastructure/pypdf/pdf_processor.py
@@ -1,5 +1,13 @@
+import io
+from pypdf import PdfReader
 from src.features.document_processing.domain.pdf_processor import PdfProcessor
 
 class PypdfProcessor(PdfProcessor):
     def process(self, file_bytes):
-        pass
+        pdf_reader = PdfReader(io.BytesIO(file_bytes))
+        text = ""
+        
+        for page in pdf_reader.pages:
+            text += page.extract_text() or ""
+
+        return text
--- src/features/document_processing/infrastructure/tiktoken/text_chunker.py
+++ src/features/document_processing/infrastructure/tiktoken/text_chunker.py
@@ -1,5 +1,7 @@
+from typing import List
 from src.features.document_processing.domain.text_chunker import TextChunker
+from  src.persistence.domain.entities import DocumentChunk
 
 class TiktokenTextChunker(TextChunker):
-    def chunk(self, max_tokens, token_overlap):
+    def chunk(self, text, max_tokens, token_overlap) -> List[DocumentChunk]:
         pass
