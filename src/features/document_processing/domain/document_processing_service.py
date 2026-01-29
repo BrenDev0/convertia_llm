6c9a6eb32829f2042f8d1928cd5a4796c9773345
Xplorers-backend <brendan.soullens@gmail.com>
2026-01-29 15:19:45 -0600

    file name change

--- src/features/document_processing/domain/document_processing_service.py
+++ src/features/document_processing/domain/document_processing_service.py
@@ -2,13 +2,13 @@ from abc import ABC, abstractmethod
 from typing import List
 from src.persistence.domain.entities import DocumentChunk
 
-class TextProcessingService(ABC):
+class DocumentProcessingService(ABC):
     @abstractmethod
-    def process_text(self, file_bytes: bytes, filename: str) -> str:
+    def process_txt(self, file_bytes: bytes, filename: str) -> List[DocumentChunk]:
         raise NotImplementedError
     
     @abstractmethod
-    def process_pdf_text(self, file_bytes: bytes) -> str:
+    def process_pdf(self, file_bytes: bytes) -> List[DocumentChunk]:
        raise NotImplementedError
     
     @abstractmethod
