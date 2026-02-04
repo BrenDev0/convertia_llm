1308c097245ccb8568e90daa251e7de7b303efb5
Brendan Lahey <lahey1991@gmail.com>
2026-02-04 12:50:25 -0600

    typing error

--- src/broker/domain/base_event.py
+++ src/broker/domain/base_event.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from  typing import Dict, Any 
+from uuid import UUID
 
 class BaseEvent(BaseModel):
-    connection_id: str
+    connection_id: UUID
     payload: Dict[str, Any]
