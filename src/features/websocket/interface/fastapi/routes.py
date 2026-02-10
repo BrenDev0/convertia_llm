eab80e5fdfe69f9abd9030124661cfbe8c7631a8
Xplorers-backend <brendan.soullens@gmail.com>
2026-02-10 09:29:09 -0600

    WIP: websicket messages

--- src/features/websocket/interface/fastapi/routes.py
+++ src/features/websocket/interface/fastapi/routes.py
@@ -3,9 +3,12 @@ import os
 import hmac
 import hashlib
 import time
-from fastapi import APIRouter, Depends, WebSocket, status, WebSocketDisconnect
-from src.features.websocket.container import WebsocketConnectionsContainer
 from uuid import UUID
+from fastapi import APIRouter, WebSocket, status, WebSocketDisconnect
+from src.features.websocket.container import WebsocketConnectionsContainer
+from src.features.websocket.domain import schemas, exceptions
+
+
 logger = logging.getLogger(__name__)
 
 router = APIRouter(
@@ -52,6 +55,8 @@ def verify_hmac_ws(signature: str, payload: str) -> bool:
     
     return True
 
+ALLOWED_MESSAGE_TYPES = ["EMBED DOCUMENT", "DELETE EMBEDDINGS"]
+
 @router.websocket("/llm/{connection_id}")
 async def async_ws_connect(
     websocket: WebSocket,
@@ -77,6 +82,38 @@ async def async_ws_connect(
     try:
         while True:
             message = await websocket.receive_json()
+
+            try:
+                parsed_message = schemas.WebsocketMessage(**message)
+
+                match parsed_message.type.upper():
+                    case "EMBED MESSAGE":
+                        return
+
+                    case "DELETE EMBEDDINGS":
+                        return
+            
+                    case _: 
+                        raise exceptions.WebsocketException(
+                            detail=f"Invalid message type: {parsed_message.type}, Allowed types {', '.join(ALLOWED_MESSAGE_TYPES)}"
+                        )
+            
+            except exceptions.WebsocketException as e:
+                error_message = schemas.WebsocketMessage(
+                    type="BAD REQUEST",
+                    data={
+                        "detail": str(e)
+                    }
+                )
+            except Exception:
+                error_message = schemas.WebsocketMessage(
+                    type="BAD REQUEST",
+                    data={
+                        "detail": "Invalid message schema" 
+                    }
+                )
+                websocket.send_json(error_message)
+
             logger.debug(f"INCOMMING MESSAGE ::: {message}")
 
     
