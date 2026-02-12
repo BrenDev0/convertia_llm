8d1492edf58b3d779c302754438dbf0682c144d7
Xplorers-backend <brendan.soullens@gmail.com>
2026-02-12 11:54:32 -0600

    removed websocket reouter and mounted new routes

--- src/app/interface/fastapi/server.py
+++ src/app/interface/fastapi/server.py
@@ -1,8 +1,8 @@
 from uuid import UUID
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.features.websocket.interface.fastapi import routes as ws_routes
-from src.features.document_processing.interface.fastapi import routes as document_router
+from src.features.document_processing.interface.fastapi import routes as document_routes, ws as document_ws
+from src.features.embeddings.interface.fastapi import routes as embeddings_routes, ws as embeddings_ws
 from src.features.websocket.container import WebsocketConnectionsContainer
 
 def create_fastapi_server():  
@@ -25,9 +25,12 @@ def create_fastapi_server():
         return {"status": "convertia llm ok"}
     
 
-    
-    app.include_router(ws_routes.router)
-    app.include_router(document_router.router)
+    app.include_router(document_routes.router)
+    app.include_router(document_ws.router)
+    app.include_router(embeddings_routes.router)
+    app.include_router(embeddings_ws.router)
+
+
 
     @app.get("/connections", tags=["Internal"])
     async def get_websocket_connections():
